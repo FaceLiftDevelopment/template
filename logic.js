970196e0713ff64036aa50a1ebabcfbb29882863
Facelift Development <faceliftdevelopment@gmail.com>
2020-01-05 13:46:17 -0700

    Color changes

--- logic.js
+++ logic.js
@@ -58,4 +58,14 @@ $(window).on('scroll', function() {
     }else {
       $('nav').removeClass('opaque-nav');
     }
-  })
+  });
+
+  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
